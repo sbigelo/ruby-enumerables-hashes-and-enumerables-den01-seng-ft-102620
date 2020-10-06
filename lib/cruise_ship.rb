7356644edd1ac9031fe1b7fb63b06fdf97cbf364
Sean Bigelow <sean.bigelow1@gmail.com>
2020-10-06 19:59:10 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,13 +7,28 @@
 # }
 
 def select_winner(passengers)
+  
+end
 
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# winner = ""
+# passengers.each do |suite, name|
+#   if suite == :suite_a && name.start_with?("A")
+#     winner = name
+#   end
+# end
  
-winner
-end
+# winner
